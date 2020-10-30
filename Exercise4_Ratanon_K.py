6be3e221b411cd2bae62ed3270e7d83f773ad13c
Naxocist <subforwork@gmail.com>
2020-10-30 23:46:57 +0700

    Create Exercise4_Ratanon_K.py

---
+++ Exercise4_Ratanon_K.py
@@ -0,0 +1,9 @@
+eng = 56
+business = 77
+com_system = 84
+com_program = 99.99
+print("--- Your Score ---")
+print("Foundation English:", eng)
+print("General Business:", business)
+print("Introduction to Computer Systems:", com_system)
+print("Computer Programming:", com_program)
