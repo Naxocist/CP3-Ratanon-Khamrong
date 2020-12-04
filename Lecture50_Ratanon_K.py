8a1b65470343c2b5ecad67e23c62733e41d84679
Naxocist <subforwork@gmail.com>
2020-12-04 22:45:35 +0700

    Create Lecture50_Ratanon_K.py

---
+++ Lecture50_Ratanon_K.py
@@ -0,0 +1,19 @@
+def AddNumber(x,y):
+    print(x+y)
+def SubtractNumber(x,y):
+    print(x-y)
+def MultiplyNumber(x,y):
+    print(x*y)
+def DivideNumber(x,y):
+    print(x/y)
+# AddNumber(3,6)
+# SubtractNumber(3,6)
+# MultiplyNumber(3,6)
+# DivideNumber(3,6)
+
+def Operation_with_Number(x,y):
+    print(x, "+", y, "=", x + y)
+    print(x, "-", y, "=", x - y)
+    print(x, "*", y, "=", x * y)
+    print(x, "/", y, "=", x / y)
+#Operation_with_Number(3,6)
